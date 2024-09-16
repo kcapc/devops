9cf3c3f383410e2ef346516bdf331092d945bdba
Ketelyn Cintia <ketelyn.cintia@gmail.cim>
2024-09-16 18:44:44 -0300

    Deixando o pylint feliz

--- devops.py
+++ devops.py
@@ -1,3 +1,5 @@
+"""Simples servidor pra hostear a pagina de devops."""
+
 from flask import Flask
 from flask import render_template
 from flask import url_for
@@ -6,6 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """Pagina padrão com o site"""
     styles = url_for('static', filename='styles.css')
     js = url_for('static', filename='index.js')
     return render_template('index.html', styles=styles, js=js)
