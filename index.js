c3c7bbe03f7a4c3d30bdaedc405c22325fc67b45
Ketelyn Cintia <ketelyn.cintia@gmail.cim>
2024-09-16 18:05:17 -0300

    Basic devops project

---
+++ index.js
@@ -0,0 +1,66 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.classList[0];
+        makeSound(buttonInnerHTML);
+        showImage(buttonInnerHTML);
+    });
+}
+
+// Detecta quando a tecla for pressionada.
+document.addEventListener("keypress", function (event) {
+    var keyPressed = event.key.toLowerCase();
+    makeSound(keyPressed);
+    showImage(keyPressed);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "d":
+            var leftCrash = new Audio("sounds/leftCrash.mp3");
+            leftCrash.play();
+            break;
+        case "e":
+            var tom1 = new Audio("sounds/tom1.mp3");
+            tom1.play();
+            break;
+        case "v":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "o":
+            var tom3 = new Audio("sounds/tom3.mp3");
+            tom3.play();
+            break;
+        case "p":
+            var tom2 = new Audio("sounds/tom2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var crash = new Audio("sounds/rightCrash.mp3");
+            crash.play();
+            break;
+        default:
+            console.log("Tecla não reconhecida: " + key);
+    }
+}
+
+function showImage(key) {
+    // Remove a classe 'active' de todos os botões para esconder as imagens
+    var allButtons = document.querySelectorAll(".drum");
+    allButtons.forEach(function (btn) {
+        btn.classList.remove("active");
+    });
+
+    // Adiciona a classe 'active' ao botão correspondente
+    var activeButton = document.querySelector("." + key);
+    if (activeButton) {
+        activeButton.classList.add("active");
+
+        // Remove a imagem após um tempo
+        setTimeout(function () {
+            activeButton.classList.remove("active");
+        }, 1000); // A imagem ficará visível por 1 segundo
+    }
+}
