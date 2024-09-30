839e086df0fc2e682b84087583db3a902c09d8e9
Ketelyn Cintia <ketelyn.cintia@gmail.cim>
2024-09-30 18:46:38 -0300

    Add pytest

---
+++ test_app.py
@@ -0,0 +1,13 @@
+import pytest # type: ignore 
+from app import app  # ajuste o import se o nome do seu arquivo for diferente
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    """Teste da rota index"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'<!doctype html>' in response.data  # Verifica se o HTML padrão está na resposta
